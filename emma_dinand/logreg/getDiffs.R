a402e43f756710d048be96b9175ac90aaca48841
EmmaExgit <ex.emma0@gmail.com>
2025-02-27 12:32:04 +0100

    getDiffs

---
+++ emma_dinand/logreg/getDiffs.R
@@ -0,0 +1,33 @@
+#getDerivatives move de vectors zoals in heave split en dan verschil
+
+getDiffs <- function(data, colname="z_wf", steps=1, power=1)  {
+  
+  n = length(data$t)
+  
+  relevant_sim_data_derivatives <- data
+  
+  
+  der0 <- c(relevant_sim_data_derivatives[1:n,colname])
+  der1<-c(0)
+  
+  for(j in 1: power)
+  {
+    for (k in 2:n)
+    {
+      der1 [k] <- der0[k] - der0[k-1]
+    }
+    der0 <- der1
+  }
+  
+  der0[1]<- NA
+
+  for (i in steps:1) {
+    col_name <- paste0("Derivative #", i, " ^", power)  # Create column name dynamically
+
+    relevant_sim_data_derivatives[[col_name]] <- der0 
+    
+    relevant_sim_data_derivatives[[col_name]] <- lag(der0, n = i-1, default = NA)  
+    
+  }
+  return(relevant_sim_data_derivatives)
+}
--- emma_dinand/logreg/prepare_clean_data.R
+++ emma_dinand/logreg/prepare_clean_data.R
@@ -1,12 +1,10 @@
+library(gridExtra)
+library(ggplot2)
+library(readxl)
+library(dplyr)
+# setwd("C:\\Users\\blomd\\Dropbox\\Documenten\\24-25 RU\\Modellenpracticum")
 
-# library(gridExtra)
-# library(ggplot2)
-# library(readxl)
-# library(dplyr)
-setwd("C:\\Users\\blomd\\Dropbox\\Documenten\\24-25 RU\\Modellenpracticum")
-# simulated_data <- read.csv("assets/M5415_10kn_JONSWAP_3m_10s/output.csv", header = TRUE, sep = ",")
-simulated_data <- read.csv("assets/data_ed_2_clean.csv", header = TRUE, sep = ",")
-
+simulated_data <- read.csv("assets/M5415_10kn_JONSWAP_3m_10s/output.csv", header = TRUE, sep = ",")
 
 # deze hierboven aanpassen naar nieuwe data
 
