5cf0fb5da93ceffffa5c91c4acb40d58566d9864
Steven Janssen <stevenjanssen1308@gmail.com>
2025-05-07 14:20:44 +0200

    spectral density

--- jorian_steven_jan/Modellenpracticum/spectraldensity.py
+++ jorian_steven_jan/Modellenpracticum/spectraldensity.py
@@ -1,7 +1,18 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from scipy import signal
 
 df = pd.read_csv(r"C:\Users\steve\OneDrive\Bureaublad\VS Code\git\Modellenpracticum\jorian_steven_jan\Modellenpracticum\Data4_added2.csv", low_memory = True)
-print(df.dtypes)
+series = df['z_wf'].to_numpy()
+f, Pxx_den = signal.periodogram(series)
+
+plt.semilogy(f, Pxx_den)
+
+
+
+plt.xlabel('frequency [Hz]')
+
+plt.ylabel('PSD [V**2/Hz]')
+
+plt.show()
