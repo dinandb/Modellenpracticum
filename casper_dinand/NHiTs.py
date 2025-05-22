e89e26e4e79359a94798007fc007a64092da0e7c
Casper <55704256+casperalgera@users.noreply.github.com>
2025-05-22 11:59:47 +0200

    22/05

--- casper_dinand/NHiTs.py
+++ casper_dinand/NHiTs.py
@@ -9,29 +9,30 @@ dt, time = [0.2]*180001, np.array(df['t'].values.flatten())
 
 
 import logging
-
 import pandas as pd
 from utilsforecast.plotting import plot_series
-
 from neuralforecast import NeuralForecast
 from neuralforecast.models import NBEATS, NHITS, LSTM
+
 
 logging.getLogger('pytorch_lightning').setLevel(logging.ERROR)
 horizon = 150
 
 # Try different hyperparmeters to improve accuracy.
 models = [
-    LSTM(input_size=2 * horizon,
-               h=horizon,                    # Forecast horizon
-               max_steps=100,                # Number of steps to train
-               scaler_type='standard',       # Type of scaler to normalize data
-               encoder_hidden_size=64,       # Defines the size of the hidden state of the LSTM
-               decoder_hidden_size=64,),     # Defines the number of hidden units of each layer of the MLP decoder
-          NHITS(h=horizon,                   # Forecast horizon
-                input_size=2 * horizon,      # Length of input sequence
-                max_steps=250,               # Number of steps to train
-                n_freq_downsample=[2, 1, 1]) # Downsampling factors for each stack output
-          ]
+    NHITS(
+        h=horizon,
+        input_size=328,
+        learning_rate=0.0010017435104832783,
+        max_steps=10000,
+        dropout_prob_theta=0.12491951168928624,
+        batch_size=64,
+        n_blocks=[3, 3, 3],
+        n_freq_downsample=[7, 5, 1],
+        accelerator="gpu",   # Enable GPU acceleration
+    )]
+
+
 nf = NeuralForecast(models=models, freq=1)
 heave_df = pd.DataFrame({'unique_id': 'heave', 'ds': np.arange(len(time)), 'y': heave})
 
@@ -39,17 +40,10 @@ nf.fit(df=heave_df.head(120000))
 
 heave_hat = nf.predict()
 
-from utilsforecast.plotting import plot_series
+
 # Create a DataFrame from the true values for plotting
 actual_df = heave_df[['unique_id', 'ds', 'y']].iloc[-horizon:]
 import matplotlib.pyplot as plt
-
-print(heave_hat)
-print(actual_df)
-
-# Plot using the proper format
-import matplotlib.pyplot as plt
-
 
 # 2) grab the last 12 actuals
 actual_tail = heave_df.iloc[-horizon:].copy()
@@ -58,7 +52,6 @@ heave_hat['ds'] = actual_tail['ds'].values
 
 # 4) plot
 plt.plot(actual_tail['ds'], actual_tail['y'], label='Actual (last 12)')
-plt.plot(heave_hat['ds'], heave_hat['LSTM'],   marker='o', label='LSTM fcst')
 plt.plot(heave_hat['ds'], heave_hat['NHITS'],  marker='x', label='NHITS fcst')
 plt.legend()
 plt.xlabel('ds')
