85a0bbc0d6f525d9eaa2e0e8af4a776c5a78079d
Jorian Weststrate <59614281+kinglolboot@users.noreply.github.com>
2025-06-25 17:26:41 +0200

    Delete Markov directory
    Opnieuw gesorteerd uploaden

--- Markov/Useful_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-file_path = r"C:\Users\joria\OneDrive\Documents\Modellenpracticum\Data.xlsx"
-out_path = r"C:\Users\joria\OneDrive\Documents\Modellenpracticum"
-
-df = pd.read_excel(file_path, header=[0,1])
-df = df[['t  [s]', 'Delta_t  [s]', 'z_wf  [m]', 'phi_wf  [rad]', 'theta_wf  [rad]', 'zeta  [m]']]
-df.to_csv(out_path + 'out.csv', index=False)
-print(df)
